bc3978d563b062de0d82674e1a74f169718f2a7a
Tumbo <33678436+carltheghost@users.noreply.github.com>
2026-09-24 10:38:13 -0400

    Phase 0 - Eli: add Source/CinematicChess/Public/Framework/CinematicChessEntity.h

---
+++ Source/CinematicChess/Public/Framework/CinematicChessEntity.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "CinematicChessEntity.generated.h"
+
+UCLASS()
+class CINEMATICCHESS_API ACinematicChessEntity : public ACharacter
+{
+    GENERATED_BODY()
+};
